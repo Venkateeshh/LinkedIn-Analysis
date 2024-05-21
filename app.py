a28436ec483760bc71df1e4f5bbdd290c188e36f
Venkatesh <rkamathvenkatesh@gmail.com>
2024-05-21 10:48:06 +0530

    fixed imports

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 import pandas as pd
 import numpy as np
 import streamlit as st
+from streamlit_option_menu import option_menu
 import streamlit.components.v1 as components
 from selenium import webdriver
 from selenium.webdriver.common.by import By
